c7adea1fafb720748ce73861a9e4eee2a649624b
Nhật Quang <1660468@student.hcmus.edu.vn>
2019-05-28 22:22:03 +0700

    tạo lại server

---
+++ MrXeOm/server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcrypt');
+const app = express();
--- client/src/app/register_KhachHang/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './registerKH.component';
