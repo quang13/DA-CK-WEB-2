f9f6a176bcb4469c787c2476a598f9a7796be20e
NhungCR <hongnhungmadridista@gmail.com>
2019-05-28 10:31:59 +0700

    + Login - Logout - Register

---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,10 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    name: string;
+    phone: string;
+    email: string;
+    address: string;
+    token: string;
+}
