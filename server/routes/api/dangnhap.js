cb6ed1fcf9e7d8629a53143f9a61f941870bbf01
Nhật Quang <1660468@student.hcmus.edu.vn>
2019-06-06 18:23:27 +0700

    đăng nhập

--- server/routes/api/dangnhap.js
+++ server/routes/api/dangnhap.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
+const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-const session = require('connect-mongo');
+const session = require('connect-mongo');// Lưu session vào mongodb
 const expressSession = require('express-session'); //Xử lí session bằng express
-TaiKhoanDB = require('../../models/taikhoan'); // Lưu session vào mongodb
+const TaiKhoan = require('../../models/taikhoan');
 
-module.exports = (app) =>{
-    app.post('api/dangnhap', (req, res, next) =>{
-        
+// địa chỉ sẽ là localhost:3000/api/taikhoan/dangnhap
+router.post('./api/taikhoan/dangnhap', (req, res) =>{
+    TaiKhoan.findOne(({TenDangNhap: req.body.TenDangNhap, MatKhau: req.body.MatKhau, BiXoa: false}), (err, data) =>{
+        if(!err) 
+        {
+            res.send({
+                success: true,
+                message: 'Đăng nhập thành công',
+            });
+            res.redirect('/');
+        }
+        else 
+        {
+            console.log('Lỗi, không tìm thấy tài khoản này');  
+        }
     });
-}
+});
